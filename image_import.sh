ceccb09d42a3102be674b3c3452acd8f8018a020
Thomas Johan Eggum <thojeggu@gmail.com>
2014-01-11 10:13:07 +0100

    Update image_import.sh

--- image_import.sh
+++ image_import.sh
@@ -1,3 +1,30 @@
+#!/bin/bash
+#Copyright (c) 2011, Thomas Eggum
+#All rights reserved.
+
+#Redistribution and use in source and binary forms, with or without
+#modification, are permitted provided that the following conditions are met:
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#    * Neither the name of the <organization> nor the
+#      names of its contributors may be used to endorse or promote products
+#      derived from this software without specific prior written permission.
+
+#THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+#ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+#WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#DISCLAIMED. IN NO EVENT SHALL Thomas Eggum BE LIABLE FOR ANY
+#DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+#(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+#ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+#SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
 say "Start image script"
 canonFolder=$(eval echo ~${SUDO_USER})/Pictures/
 mobileFolder=$(eval echo ~${SUDO_USER})/Pictures/mobilbilder
@@ -16,16 +43,14 @@ while [[ true ]]; do
 		fi
 	    (( i++ ))
 		
-	    #flytte fil til mappestruktur
-
-	    # sjekke om canon?
+	    #differ camera models
 	    cameraModel=$(exiftool "$file" | grep "Camera Model")
 		basePath=$mobileFolder
 		if [[ $cameraModel == *Canon* ]]; then
 			basePath=$canonFolder
 		fi
 
-		#START HANDLING EXIF
+		#extract exif for destination folder
 		f="${file##*/}"
 		imageFolderName=$basePath/NO_EXIF_FILES #defaults
 		response=$(exiftool -CreateDate "$file") 
@@ -41,15 +66,13 @@ while [[ true ]]; do
 			mkdir -p "$imageFolderName"
 		fi	
 
-		#NOW deal with duplicates
-		#Create destination file name
+		#create unique file names
 		checksumString=$(cksum "$file")
 		checksumArray=($checksumString)
 		checksum=${checksumArray[0]}
 		destinationFile=$imageFolderName/$checksum"_"$f
 
-		#Handle copy image
-
+		#move images to folder structure
 		if [ -f "$destinationFile" ]; then
 			rm "$file"
 			fileDiff=$(diff $destinationFile "$file")
